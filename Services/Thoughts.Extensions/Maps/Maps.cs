5d7bcaa4822f271924af8614c7a50e8d452f6034
Алексей Нестеров <nester.a.l@yandex.ru>
2022-06-03 12:50:26 +0300

    добавлены методы маппинга в домейн для комментария и для фаила тесты не работают

--- Services/Thoughts.Extensions/Maps/Maps.cs
+++ Services/Thoughts.Extensions/Maps/Maps.cs
@@ -1,16 +1,27 @@
 ﻿using AutoMapper;
+using Thoughts.Domain.Base.Entities;
 
 using CategoryDomain = Thoughts.Domain.Base.Entities.Category;
 using StatusDomain = Thoughts.Domain.Base.Entities.Status;
+using CommentDomain = Thoughts.Domain.Base.Entities.Comment;
 using CategoryDal = Thoughts.DAL.Entities.Category;
 using StatusDal = Thoughts.DAL.Entities.Status;
+using CommentDal = Thoughts.DAL.Entities.Comment;
+using File = Thoughts.DAL.Entities.File;
 
 namespace Thoughts.Extensions.Maps
 {
+    /// <summary>Класс маппинга из БД в Domain</summary>
     public static class Maps
     {
+        //зачем категории хранить в себе коллекцию постов, которые в неё входят?
+        //зачем ролям хранить коллекцию юзеров?
+        //зачем тэгам хранить коллекцию постов?
 
-        public static StatusDomain ToDomain(this StatusDal statusDal)
+        /// <summary>Преобразование статуса из БД в Domain форму</summary>
+        /// <param name="dalEntity">Статус из БД</param>
+        /// <returns>Статус Domian</returns>
+        public static StatusDomain ToDomain(this StatusDal dalEntity)
         {
             var config = new MapperConfiguration(cfg => cfg.CreateMap<StatusDal, StatusDomain>()
                 .ForMember("Id", opt => opt.MapFrom(s => s.Id))
@@ -18,21 +29,60 @@ namespace Thoughts.Extensions.Maps
 
             var mapper = new Mapper(config);
 
-            return mapper.Map<StatusDomain>(statusDal);
+            return mapper.Map<StatusDomain>(dalEntity);
         }
 
-        // todo: пока не работает =(
-        public static CategoryDomain ToDomain(this CategoryDal categoryDal)
+        /// <summary>Преобразование категории из БД в Domain форму</summary>
+        /// <param name="dalEntity">Категория из БД</param>
+        /// <returns>Категория Domian</returns>
+        public static CategoryDomain ToDomain(this CategoryDal dalEntity)
         {
             var config = new MapperConfiguration(cfg => cfg.CreateMap<CategoryDal, CategoryDomain>()
                 .ForMember("Id", opt => opt.MapFrom(s => s.Id))
                 .ForMember("Name", opt => opt.MapFrom(s => s.Name))
                 .ForMember("Status", opt => opt.MapFrom(s => s.Status))
-                .ForMember("Posts", opt => opt.MapFrom(s => s.Posts)));
+                .ForMember("Posts", opt => opt.MapFrom(s => s.Posts))); //зачем коллекцию постов хранить?
 
             var mapper = new Mapper(config);
 
-            return mapper.Map<CategoryDomain>(categoryDal);
+            return mapper.Map<CategoryDomain>(dalEntity);
+        }
+
+
+        /// <summary>Преобразование комментария из БД в Domain форму</summary>
+        /// <param name="dalEntity">Комментарий из БД</param>
+        /// <returns>Комментарий Domian</returns>
+        public static CommentDomain ToDomain(this CommentDal dalEntity)
+        {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<CommentDal, CommentDomain>()
+                .ForMember("Id", opt => opt.MapFrom(s => s.Id))
+                .ForMember("Date", opt => opt.MapFrom(s => s.Date))
+                .ForMember("Post", opt => opt.MapFrom(s => s.Post)) //здесь нужно метод маппинга добавить
+                .ForMember("User", opt => opt.MapFrom(s => s.Post)) //здесь нужно метод маппинга добавить
+                .ForMember("Body", opt => opt.MapFrom(s => s.Body))
+                .ForMember("ParentComment", opt => opt.MapFrom(s => s.ParentComment.ToDomain())) // здесь у нас рекурсия пошла
+                .ForMember("ChildrenComment", opt => opt.MapFrom(s => s.ChildrenComment)) // здесь у нас тоже рекурсия будет
+                .ForMember("isDeleted", opt => opt.MapFrom(s => s.IsDeleted)));
+
+            var mapper = new Mapper(config);
+
+            return mapper.Map<CommentDomain>(dalEntity);
+        }
+
+        /// <summary>Преобразование фаила из БД в Domain форму</summary>
+        /// <param name="dalEntity">Фаил из БД</param>
+        /// <returns>Фаил Domian</returns>
+        public static FileModel ToDomain(this File dalEntity)
+        {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<File, FileModel>()
+                .ForMember("Id", opt => opt.MapFrom(s => s.Id))
+                .ForMember("Name", opt => opt.MapFrom(s => s.Name))
+                .ForMember("Description", opt => opt.MapFrom(s => s.FileDescription))
+                .ForMember("Content", opt => opt.MapFrom(s => s.FileBody)));
+
+            var mapper = new Mapper(config);
+
+            return mapper.Map<FileModel>(dalEntity);
         }
     }
 }
--- Tests/Thoughts.Tests/Services/Thoughts.Extensions/Maps/MapsTests.cs
+++ Tests/Thoughts.Tests/Services/Thoughts.Extensions/Maps/MapsTests.cs
@@ -26,22 +26,5 @@ namespace Thoughts.Tests.Services.ThoughtsExtensions.Maps
             Assert.Equal(result.Id, source.Id);
             Assert.Equal(result.Name, source.Name);
         }
-
-        //[TestMethod]
-        //public void CategoryDal_ToDomain_Test()
-        //{
-        //    var source = new CategoryDal()
-        //    {
-        //        Id = 1,
-        //        Name = "Test",
-        //        Status = new DAL.Entities.Status("Test") { Id = 11 },
-        //    };
-        //    var result = source.ToDomain();
-
-        //    Assert.True(result is CategoryDomain);
-        //    Assert.Equal(result.Id, source.Id);
-        //    Assert.Equal(result.Name, source.Name);
-        //    Assert.Equal(result.Status.Name, source.Status.Name);
-        //}
     }
 }
