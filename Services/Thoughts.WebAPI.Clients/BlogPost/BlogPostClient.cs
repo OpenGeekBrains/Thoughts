52f958c9f10ff0ed4e4b9c2b31edff2cd626937d
Алексей Нестеров <nester.a.l@yandex.ru>
2022-06-03 21:34:30 +0300

    Базовый клиент

---
+++ Services/Thoughts.WebAPI.Clients/BlogPost/BlogPostClient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
+
+using Thoughts.Interfaces;
+using Thoughts.WebAPI.Clients.Base;
+
+namespace Thoughts.WebAPI.Clients.BlogPost
+{
+    public class BlogPostClient : BaseClient//, IBlogPostManager
+    {
+        protected BlogPostClient(HttpClient client, ILogger<BlogPostClient> logger) : base(client, "")
+        {
+        }
+    }
+}
