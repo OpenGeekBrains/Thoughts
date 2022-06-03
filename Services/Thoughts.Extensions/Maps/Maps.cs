e114a69ced762aea975c4fcae56bfcf59b7a79e4
Алексей Нестеров <nester.a.l@yandex.ru>
2022-06-03 12:00:38 +0300

    добавлен проект с маппером, добавлен проект тестов  пока ничего не работает

---
+++ Services/Thoughts.Extensions/Maps/Maps.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+
+using CategoryDomain = Thoughts.Domain.Base.Entities.Category;
+using CategoryDal = Thoughts.DAL.Entities.Category;
+
+namespace Thoughts.Extensions.Maps
+{
+    public static class Maps
+    {
+        public static CategoryDomain ToDomain(this CategoryDal categoryDal)
+        {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<CategoryDal, CategoryDomain>()
+                .ForMember("Id", opt => opt.MapFrom(s => s.Id))
+                .ForMember("Name", opt => opt.MapFrom(s => s.Name))
+                .ForMember("Status", opt => opt.MapFrom(s => s.Status))
+                .ForMember("Posts", opt => opt.MapFrom(s => s.Posts)));
+
+            var mapper = new Mapper(config);
+
+            return mapper.Map<CategoryDomain>(categoryDal);
+        }
+    }
+}
---
+++ Tests/Thoughts.Tests/Services/Thoughts.Extensions/Maps/MapsTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Thoughts.Extensions.Maps;
+using Assert = Xunit.Assert;
+
+using CategoryDomain = Thoughts.Domain.Base.Entities.Category;
+using CategoryDal = Thoughts.DAL.Entities.Category;
+
+namespace Thoughts.Tests.Services.ThoughtsExtensions.Maps
+{
+    [TestClass]
+    public class MapsTests
+    {
+        [TestMethod]
+        public void CategoryDal_ToDomain_Test()
+        {
+            var source = new CategoryDal()
+            {
+                Id = 1,
+                Name = "Test",
+                Status = new DAL.Entities.Status("Test") { Id = 11 },
+            };
+            var result = source.ToDomain();
+
+            Assert.True(result is CategoryDomain);
+            Assert.Equal(result.Id, source.Id);
+            Assert.Equal(result.Name, source.Name);
+            Assert.Equal(result.Status.Name, source.Status.Name);
+        }
+    }
+}
