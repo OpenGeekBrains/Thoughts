42f92cf5ff1fb3cff756fba4ca1148bd419d2668
Aleksey N <nester.a.l@yandex.ru>
2022-07-04 22:30:47 +0300

    перемещение интерфейса в новую папку

---
+++ Services/Thoughts.Interfaces.Base/Mapping/IMapper.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Thoughts.Interfaces.Base.Mapping
+{
+    public interface IMapper<TSource, TResult>
+    {
+
+        [return: NotNullIfNotNull("item")]
+        TResult? Map(TSource? item);
+
+        [return: NotNullIfNotNull("item")] // todo: Посмотреть варианты реализации
+        TSource? MapBack(TResult? item);
+    }
+}
