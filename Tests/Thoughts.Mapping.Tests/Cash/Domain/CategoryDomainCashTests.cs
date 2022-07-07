7c810f40c953773d8b9630ec5834cf0f2e11c659
Aleksey N <nester.a.l@yandex.ru>
2022-07-07 22:30:37 +0300

    тестирование единичных элеменетов кэша (тестировать все не имеет смысла, потому что они устроены одниково)

---
+++ Tests/Thoughts.Mapping.Tests/Cash/Domain/CategoryDomainCashTests.cs
@@ -0,0 +1,24 @@
+﻿using Thoughts.Extensions.Mapping.Cash.Domain;
+
+using Xunit;
+
+namespace Thoughts.Mapping.Tests.Cash.Domain
+{
+    public class CategoryDomainCashTests
+    {
+        CategoryDomainCash _category = new CategoryDomainCash();
+
+        [Fact]
+        public void CashCreated()
+        {
+            Assert.True(_category.Cash.Count == 0);
+        }
+
+        [Fact]
+        public void CashDisposed()
+        {
+            _category.Dispose();
+            Assert.True(_category.Cash is null);
+        }
+    }
+}
