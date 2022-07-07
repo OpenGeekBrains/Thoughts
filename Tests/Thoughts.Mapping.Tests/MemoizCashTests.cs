f64e991cb415ba2c1dc1cfe7fd4b2279738474f8
Aleksey N <nester.a.l@yandex.ru>
2022-07-07 22:31:13 +0300

    тестирование кэша мемоизации

---
+++ Tests/Thoughts.Mapping.Tests/MemoizCashTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Thoughts.Extensions.Mapping;
+
+using Xunit;
+
+namespace Thoughts.Mapping.Tests
+{
+    public class MemoizCashTests : IDisposable
+    {
+        MemoizCash _cash;
+
+        public MemoizCashTests()
+        {
+            _cash = new MemoizCash();
+        }
+        public void Dispose()
+        {
+            _cash.Dispose();
+        }
+
+        [Fact]
+        public void CashCreated()
+        {
+            var rolesDom = _cash.RolesDomain;
+            Assert.NotNull(rolesDom);
+            Assert.NotNull(_cash.RolesDomain);
+        }
+    }
+}
