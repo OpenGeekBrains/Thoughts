ce3bc6889f7c6178c6fdc878f0a571f1ebb195e9
Aleksey N <nester.a.l@yandex.ru>
2022-07-09 16:32:39 +0300

    попытка сделать внедрение зависимостей в тест

---
+++ Tests/Thoughts.Mapping.Tests/Maps/CategoryMapperTests.cs
@@ -0,0 +1,24 @@
+﻿using Thoughts.DAL.Entities;
+using Thoughts.Interfaces.Base.Mapping;
+
+using Xunit;
+
+using CategoryDom = Thoughts.Domain.Base.Entities.Category;
+namespace Thoughts.Mapping.Tests.Maps
+{
+    public class CategoryMapperTests
+    {
+        private readonly IMapper<CategoryDom, Category> _mapper;
+
+        public CategoryMapperTests(IMapper<CategoryDom, Category> mapper)
+        {
+            _mapper = mapper;
+        }
+
+        [Fact]
+        public void MapperIsNotNull()
+        {
+            Assert.True(_mapper is not null);
+        }
+    }
+}
---
+++ Tests/Thoughts.Mapping.Tests/Startup.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using CategoryDom = Thoughts.Domain.Base.Entities.Category;
+using CommentDom = Thoughts.Domain.Base.Entities.Comment;
+using PostDom = Thoughts.Domain.Base.Entities.Post;
+using RoleDom = Thoughts.Domain.Base.Entities.Role;
+using TagDom = Thoughts.Domain.Base.Entities.Tag;
+using UserDom = Thoughts.Domain.Base.Entities.User;
+using Thoughts.DAL.Entities;
+using Thoughts.Interfaces.Base.Mapping;
+using Thoughts.Extensions.Mapping.Maps;
+using Thoughts.Interfaces.Mapping;
+using Thoughts.Extensions.Mapping;
+
+namespace Thoughts.Mapping.Tests
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<IMemoizCash, MemoizCash>();
+            services.AddScoped<IMapper<CategoryDom, Category>, CategoryMapper>();
+            services.AddScoped<IMapper<CommentDom, Comment>, CommentMapper>();
+            services.AddScoped<IMapper<PostDom, Post>, PostMapper>();
+            services.AddScoped<IMapper<RoleDom, Role>, RoleMapper>();
+            services.AddScoped<IMapper<TagDom, Tag>, TagMapper>();
+            services.AddScoped<IMapper<UserDom, User>, UserMapper>();
+        }
+    }
+}
