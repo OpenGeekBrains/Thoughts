b1f3d59b2e50c5018160ac890ae9fe1fa810b0dd
Infarh <shmachilin@yandex.ru>
2022-07-09 21:43:20 +0300

    Попытка доработки таг-хелпера

---
+++ Tests/Thoughts.Services.Tests/DecodeEscapedStringTest.cs
@@ -0,0 +1,47 @@
+﻿using System.Globalization;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Thoughts.Services.Tests;
+
+[TestClass]
+public class DecodeEscapedStringTest
+{
+    [TestMethod]
+    public void DecodeTest()
+    {
+        const string source_string = "Body6. Aenean ornare velit lacus, ac varius enim lorem "
+            + "ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem "
+            + "feugiat tempus aliquam.&lt;br/&gt;&lt;img src=&quot;"
+            + "https://catholicsar.ru/wp-content/uploads/1516116812_ryzhiy-maine-coon.jpg&quot; "
+            + "alt=&quot;&#x43A;&#x430;&#x440;&#x442;&#x438;&#x43D;&#x43A;&#x430;&quot;/&gt;";
+
+        var regex = new Regex(@"&[^;]+;");
+
+        var result = regex.Replace(source_string, match =>
+        {
+            return match.Value.TrimStart('&').TrimEnd(';') switch
+            {
+                "lt" => "<",
+                "gt" => ">",
+                "quot" => "\"",
+                { } s when s.StartsWith("#x") => DecodeSymbol(short.Parse(match.Value[2..], NumberStyles.HexNumber)),
+                _ => ""
+            };
+        });
+
+        static string DecodeSymbol(short Code)
+        {
+            var c = (char)((Code - 0x43A) + 'а');
+            return c.ToString();
+        }
+
+        const string expected_string = "Body6. Aenean ornare velit lacus, ac varius enim lorem "
+            + "ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem "
+            + "feugiat tempus aliquam.<br/><img src=\""
+            + "https://catholicsar.ru/wp-content/uploads/1516116812_ryzhiy-maine-coon.jpg\" "
+            + "alt=\"картинка\"/>";
+
+        Assert.Equals(expected_string, result);
+    }
+}
--- UI/Thoughts.UI.MVC/TagHelpers/RawhtmlTagHelper.cs
+++ UI/Thoughts.UI.MVC/TagHelpers/RawhtmlTagHelper.cs
@@ -1,11 +1,15 @@
-﻿namespace Thoughts.UI.MVC.TagHelpers;
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace Thoughts.UI.MVC.TagHelpers;
 
 /// <summary>
 /// Таг-хелпер отображения html в сыром виде
 /// </summary>
 public class RawhtmlTagHelper : TagHelper
 {
-    private IHtmlHelper _htmlHelper { get; }
+    private IHtmlHelper _htmlHelper;
+
     public RawhtmlTagHelper(IHtmlHelper htmlHelper)
     {
         _htmlHelper = htmlHelper ?? throw new ArgumentNullException(nameof(htmlHelper));
@@ -16,8 +20,21 @@ public class RawhtmlTagHelper : TagHelper
     /// </summary>
     public string Text { get; set; }
 
-    public override void Process(TagHelperContext context, TagHelperOutput output)
+    [ViewContext, HtmlAttributeNotBound]
+    public ViewContext ViewContext { get; set; }
+
+    public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
     {
+        var content = await output.GetChildContentAsync();
+        var content_html = content.GetContent();
+
+        if (content_html is { Length: > 0 })
+        {
+            //output.Content.AppendHtml(content_html);
+            output.Content.SetHtmlContent(new HtmlString(content_html));
+            return;
+        }
+
         output.TagName = null;
         var text = _htmlHelper.Raw(Text);
         output.Content.SetHtmlContent(text);
