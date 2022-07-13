1eb59856cd17daa059ec955d5b6d7f5bfc85c0be
Aleksey N <nester.a.l@yandex.ru>
2022-07-13 21:18:08 +0300

    приспособы

---
+++ Tests/Thoughts.Maps.Tests/Fixtures/DbFixture.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+using Microsoft.EntityFrameworkCore;
+
+using Thoughts.DAL;
+using Thoughts.Services.Data;
+
+namespace Thoughts.Maps.Tests.Fixtures
+{
+    public class DbFixture : IDisposable
+    {
+        public ThoughtsDB DB { get; private set; }
+        public DbFixture()
+        {
+            var builder = new DbContextOptionsBuilder<ThoughtsDB>();
+            builder.UseInMemoryDatabase("InMemoryDb");
+
+            var options = builder.Options;
+
+            DB = new ThoughtsDB(options);
+
+            DB.Database.EnsureDeleted();
+            DB.Database.EnsureCreated();
+
+            DB.Categories.AddRange(TestDbData.Categories);
+            DB.SaveChanges();
+            if (!DB.Categories.Any())
+            {
+                throw new Exception("Categories Empty");
+            }
+        }
+
+        public void Dispose()
+        {
+            DB?.Dispose();
+        }
+    }
+}
