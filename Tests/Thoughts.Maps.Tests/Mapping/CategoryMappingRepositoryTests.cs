a851007d3bfa22204c5171e64438adf00c2a0657
Aleksey N <nester.a.l@yandex.ru>
2022-07-13 22:38:12 +0300

    тестируем маппинг репозиторий категорий

--- Tests/Thoughts.Maps.Tests/Mapping/CategoryMappingRepositoryTests.cs
+++ Tests/Thoughts.Maps.Tests/Mapping/CategoryMappingRepositoryTests.cs
@@ -1,5 +1,4 @@
-﻿
-using Thoughts.Maps.Tests.Fixtures;
+﻿using Thoughts.Maps.Tests.Fixtures;
 using Thoughts.Services.Mapping;
 using CategoryDom = Thoughts.Domain.Base.Entities.Category;
 using CategoryDAL = Thoughts.DAL.Entities.Category;
@@ -9,6 +8,9 @@ using Moq;
 using Microsoft.Extensions.Logging;
 using Thoughts.Services.InSQL;
 using Thoughts.Extensions.Maps;
+using System.Collections.Generic;
+using System;
+using System.Linq;
 
 namespace Thoughts.Maps.Tests.Mapping
 {
@@ -20,6 +22,7 @@ namespace Thoughts.Maps.Tests.Mapping
         DbRepository<CategoryDAL> _dbRepository;
         Mock<ILogger<DbRepository<CategoryDAL>>> _dbRepoMock;
         CategoryMapper _mapper;
+
         public CategoryMappingRepositoryTests(DbFixture fixture)
         {
             _fixture = fixture;
@@ -64,5 +67,99 @@ namespace Thoughts.Maps.Tests.Mapping
             var result = await _repo.ExistId(id);
             Assert.False(result);
         }
+
+        [Theory]
+        [InlineData(1, "Category1")]
+        [InlineData(2, "Category2")]
+        [InlineData(3, "Category3")]
+        public async void ExistTest_ReturnTrue(int id, string name)
+        {
+            var item = new CategoryDom()
+            {
+                Id = id,
+                Name = name,
+            };
+
+            var result = await _repo.Exist(item);
+
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData(4, "Category4")]
+        public async void ExistTest_ReturnFalse(int id, string name)
+        {
+            var item = new CategoryDom()
+            {
+                Id = id,
+                Name = name,
+            };
+
+            var result = await _repo.Exist(item);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public async void ExistTest_ThrownException_WithNullAgrument()
+        {
+            bool catched = false;
+            try
+            {
+                var result = await _repo.Exist(null);
+            }
+            catch(ArgumentNullException e)
+            {
+                catched = true;
+                Assert.True(e is ArgumentNullException);
+            }
+            Assert.True(catched);
+        }
+
+        [Fact]
+        public async void GetCountTest()
+        {
+            var result = await _repo.GetCount();
+
+            Assert.Equal(3, result);
+        }
+
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(1, 2)]
+        [InlineData(2, 1)]
+        public async void GetTest(int skip, int count)
+        {
+            var result = await _repo.Get(skip, count);
+
+            bool correct = true;
+
+            switch (result.Count())
+            {
+                case 1:
+                    if (result.FirstOrDefault(s => s.Id == 3) is not null)
+                        Assert.True(result.FirstOrDefault(s => s.Id == 3) is CategoryDom);
+                    else if (result.FirstOrDefault(s => s.Id == 2) is not null)
+                    {
+                        Assert.True(result.FirstOrDefault(s => s.Id == 2) is CategoryDom);
+                    }
+                    else correct = false;
+                    break;
+                case 2:
+                    if(result.FirstOrDefault(s => s.Id == 2) is not null)
+                    {
+                        Assert.True(result.FirstOrDefault(s => s.Id == 3) is CategoryDom);
+                        Assert.True(result.FirstOrDefault(s => s.Id == 2) is CategoryDom);
+                    }
+                    else correct = false;
+                    break;
+                default:
+                    correct = false;
+                    break;
+            }
+
+            Assert.True(correct);
+        }
+
     }
 }
