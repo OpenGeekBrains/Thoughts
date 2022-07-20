92fd999362a0ec98cf750a8687c8209f1d588ae3
Aleksey N <nester.a.l@yandex.ru>
2022-07-20 22:27:39 +0300

    создание ДБ приспособы

---
+++ Tests/Thoughts.Services.NewTests/Fixtures/DBFixture.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Thoughts.DAL;
+using Thoughts.Services.Data;
+
+namespace Thoughts.Services.NewTests.Fixtures
+{
+    public class DbFixture : IDisposable
+    {
+        public ThoughtsDB DB { get; private set; }
+        public DbFixture()
+        {
+            var builder = new DbContextOptionsBuilder<ThoughtsDB>();
+            builder.UseInMemoryDatabase("InMemoryDb");
+            builder.EnableSensitiveDataLogging();
+
+            var options = builder.Options;
+
+            DB = new ThoughtsDB(options);
+
+            DB.Database.EnsureDeleted();
+            DB.Database.EnsureCreated();
+            DB.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+
+            DB.Categories.AddRange(TestDbData.Categories);
+            //DB.Comments.AddRange(TestDbData.Comments);
+            DB.Posts.AddRange(TestDbData.Posts);
+            //DB.Roles.AddRange(TestDbData.Roles);
+            DB.Tags.AddRange(TestDbData.Tags);
+            DB.Users.AddRange(TestDbData.Users);
+            DB.SaveChanges();
+            if (!DB.Categories.Any()) throw new Exception("Categories Set in DB is Empty");
+            if (!DB.Comments.Any()) throw new Exception("Comments Set in DB is Empty");
+            if (!DB.Posts.Any()) throw new Exception("Posts Set in DB is Empty");
+            if (!DB.Roles.Any()) throw new Exception("Roles Set in DB is Empty");
+            if (!DB.Tags.Any()) throw new Exception("Tags Set in DB is Empty");
+            if (!DB.Users.Any()) throw new Exception("Users Set in DB is Empty");
+        }
+
+        public void Dispose()
+        {
+            DB?.Dispose();
+        }
+    }
+}
