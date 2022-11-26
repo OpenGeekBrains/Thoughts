20f000b3bdca79f43d6f5541c2134d2503b31645
RonzhinLeonid <jiehuh87@yandex.ru>
2022-11-26 23:55:49 +0300

    Добавлен токен

---
+++ DTO/Identity/TokenResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO.Identity
+{
+    public sealed class TokenResponse
+    {
+        public string Token { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
